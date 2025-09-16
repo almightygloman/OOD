a6570425529d89cbcaf37dc7f81e824589c9614d
almmightygloman <zachivanovs@gmail.com>
2025-09-15 20:25:17 -0400

    Monthly occursOn works

--- HW1/Main.java
+++ HW1/Main.java
@@ -2,8 +2,9 @@ package HW1;
 import java.time.LocalDate;
 public class Main {
     public static void main(String[]args){
-        Onetime ot = new Onetime("tooth pulled", LocalDate.of(2025,12,25));
-        Appointment daily = new Daily(null, LocalDate.of(2022, 12, 2));
+        Onetime ot = new Onetime("tooth pulled", LocalDate.of(2025,12,26));
+        Appointment monthly = new Monthly(null, LocalDate.of(2025, 11, 30));
         System.out.println(ot.occursOn(2025, 12, 26));
+        System.out.println(monthly.occursOn(2025, 12, 31));
     }
 }
--- HW1/Monthly.java
+++ HW1/Monthly.java
@@ -11,11 +11,12 @@ public class Monthly extends Appointment{
     @Override
     public boolean occursOn(int year, int month, int day) {
         LocalDate temp = LocalDate.of(year, month, day);
+
         //make sure the date we are checking is after/on the appointment date
         if(temp.isBefore(getDate())) return false;
         //check if the days are the last of the month
-        if(temp.getMonthValue() == temp.getMonth().length(temp.isLeapYear()) &&
-            getDate().getMonthValue() == getDate().getMonth().length(getDate().isLeapYear())){
+        if(temp.getDayOfMonth() == temp.getMonth().length(temp.isLeapYear()) &&
+            getDate().getDayOfMonth() == getDate().getMonth().length(getDate().isLeapYear())){
                 return true;
         }
 
