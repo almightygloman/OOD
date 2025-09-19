23e0308e1005bd44e7205a6263ded45e35e9fd4a
almmightygloman <zachivanovs@gmail.com>
2025-09-19 09:49:32 -0400

    consolidated occursOn function in Monthly subclass

--- HW1/Monthly.java
+++ HW1/Monthly.java
@@ -11,17 +11,15 @@ public class Monthly extends Appointment{
     @Override
     public boolean occursOn(int year, int month, int day) {
         LocalDate temp = LocalDate.of(year, month, day);
-
+        LocalDate apDate = getDate();
         //make sure the date we are checking is after/on the appointment date
         if(temp.isBefore(getDate())) return false;
-        //check if the days are the last of the month
-        if(temp.getDayOfMonth() == temp.getMonth().length(temp.isLeapYear()) &&
-            getDate().getDayOfMonth() == getDate().getMonth().length(getDate().isLeapYear())){
-                return true;
-        }
+        //check if the days are the last of the month if the DOTM aren't equal
+        return temp.getDayOfMonth() == apDate.getDayOfMonth() || isLastDay(apDate) == isLastDay(temp);
+    }
 
-
-        return temp.getDayOfMonth() == getDate().getDayOfMonth();
+    public boolean isLastDay(LocalDate d){
+        return d.getDayOfMonth() == d.getMonth().length(d.isLeapYear());
     }
     
 }
